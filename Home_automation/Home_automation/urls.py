86fa438795b9aa80a1227f7d74d88e9dce0f97fa
jonathan <jonathan@jonathananderson.se>
2016-01-10 01:02:42 +0100

    changed to django

--- Home_automation/Home_automation/urls.py
+++ Home_automation/Home_automation/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from application.views import index, switch, wire
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'Home_automation.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', view=index, name="index"),
+    url(r'^switch/(?P<nr>[0-9]+)/(?P<on>[0-9])/$', view=switch, name="switch"),
+    url(r'^wire/(?P<on>[0-9])/$', view=wire, name="wire"),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- Home_automation/application/views.py
+++ Home_automation/application/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from plugs.radioplugs import send_code
+from plugs.wiredplugs import turn_on, turn_off
 
-# Create your views here.
+
+@login_required
+def index(request):
+    return render(request, 'application/index.html')
+
+
+@login_required
+def switch(request, on, nr):
+    send_code(True, on, nr)
+    return render(request, 'application/index.html')
+
+
+@login_required
+def wire(request, on):
+    wire_port = 18
+    if on:
+        turn_on(wire_port)
+    else:
+        turn_off(wire_port)
+    return render(request, 'application/index.html')
