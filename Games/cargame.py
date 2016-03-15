993228610bc1d56e2d10adb7ccd3fa79c7c07bea
jonathan <jonathan@jonathananderson.se>
2016-03-15 15:40:10 +0100

    test add to path

--- Games/cargame.py
+++ Games/cargame.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.join(sys.path[0], '..'))
 
 import time
-from Adafruit.Adafruit_CharLCD import Adafruit_CharLCD
+from Adafruit.Adafruit_CharLCD.Adafruit_CharLCD import Adafruit_CharLCD
 import random
 
 __author__ = 'jonathan'
