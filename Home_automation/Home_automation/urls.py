e96d6e59a797972155b341777114214e0f0fa861
jonathan <jonathan@jonathananderson.se>
2016-07-06 23:23:25 +0200

    Bugfix

--- Home_automation/Home_automation/urls.py
+++ Home_automation/Home_automation/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # url(r'^$', 'Home_automation.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', view=index, name="index"),
-    url(r'^api/switch/(?P<pk>[0-9]+)/(?P<status>[^/]+)/$', view=switch, name="switch"),
+    url(r'^api/switch/(?P<pk>[0-9]+)/(?P<button_pk>[^/]+)/$', view=switch, name="switch"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', login, {'template_name': 'application/login.html'}),
     url(r'^accounts/logout/$', logout),
