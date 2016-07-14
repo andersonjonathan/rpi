75691a7d76f68783605032fb3460101aa3731efd
jonathan <jonathan@jonathananderson.se>
2016-07-14 21:49:36 +0200

    Quick fix for django admin

--- Home_automation/application/admin.py
+++ Home_automation/application/admin.py
@@ -8,22 +8,74 @@ from .models import (
     RadioSignal,
     RadioPlug,
     WiredPlug,
-    Button, WiredButton, RadioButton)
+    Button, WiredButton, RadioButton, IRButton, IRDevice)
+
+
+class ScheduleSlotAdmin(admin.StackedInline):
+    model = ScheduleSlot
 
 
 class ScheduleAdmin(admin.ModelAdmin):
-    pass
+    model = Schedule
+    inlines = [ScheduleSlotAdmin]
 
 
-admin.site.register(Schedule)
-admin.site.register(ScheduleSlot)
-admin.site.register(Plug)
+class RadioSignalAdmin(admin.TabularInline):
+    model = RadioSignal
+
+
+class RadioProtocolAdmin(admin.ModelAdmin):
+    model = RadioProtocol
+
+    inlines = [RadioSignalAdmin]
+    list_display = ('name',)
+    list_display_links = ('name',)
+    search_fields = ['name']
+
+
+class ButtonAdmin(admin.TabularInline):
+    model = Button
+
+
+class PlugAdmin(admin.ModelAdmin):
+    model = Plug
+
+    inlines = [ButtonAdmin]
+    list_display = ('name',)
+    list_display_links = ('name',)
+    search_fields = ['name']
+
+
+class WiredButtonAdmin(ButtonAdmin):
+    model = WiredButton
+
+
+class WiredPlugAdmin(PlugAdmin):
+    model = WiredPlug
+    inlines = [WiredButtonAdmin]
+
+
+class RadioButtonAdmin(ButtonAdmin):
+    model = RadioButton
+
+
+class RadioPlugAdmin(PlugAdmin):
+    model = RadioPlug
+    inlines = [RadioButtonAdmin]
+
+
+class IRButtonAdmin(ButtonAdmin):
+    model = IRButton
+
+
+class IRDeviceAdmin(PlugAdmin):
+    model = IRDevice
+    inlines = [IRButtonAdmin]
+
+admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(RadioTransmitter)
-admin.site.register(RadioProtocol)
-admin.site.register(RadioSignal)
-admin.site.register(RadioPlug)
-admin.site.register(WiredPlug)
-admin.site.register(Button)
-admin.site.register(WiredButton)
-admin.site.register(RadioButton)
+admin.site.register(RadioProtocol, RadioProtocolAdmin)
+admin.site.register(RadioPlug, RadioPlugAdmin)
+admin.site.register(WiredPlug, WiredPlugAdmin)
+admin.site.register(IRDevice, IRDeviceAdmin)
 # Register your models here.
