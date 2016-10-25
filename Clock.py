4d1afd3fb6c430454595db510a231e1fdbeb2d6a
jonathan <jonathan@jonathananderson.se>
2016-10-25 02:09:19 +0200

    test the clock

--- Clock.py
+++ Clock.py
@@ -5,16 +5,17 @@ from time import sleep
 mcp = Adafruit_MCP230XX(address = 0x21, num_gpios = 16) # MCP23017
 
 # Set pins 0, 1 and 2 to output (you can set pins 0..15 this way)
-mcp.config(0, mcp.OUTPUT)
+mcp.config(7, mcp.OUTPUT)
+mcp.config(8, mcp.OUTPUT)
 
 # Read input pin and display the results
 print "Pin 3 = %d" % (mcp.input(3) >> 3)
 
 # Python speed test on output 0 toggling at max speed
 print "Starting blinky on pin 0 (CTRL+C to quit)"
-
+mcp.output(8, 1)  # Pin 0 High
 while (True):
-    mcp.output(0, 1)  # Pin 0 High
+    mcp.output(7, 1)  # Pin 0 High
     sleep(1)
-    mcp.output(0, 0)  # Pin 0 Low
+    mcp.output(7, 0)  # Pin 0 Low
     sleep(1)
